55f5d8e955ecfd58eb9cc1e6fec5c3eb7ea54695
Amidatelion <63753686+amidatelion@users.noreply.github.com>
2025-05-05 23:20:29 -0400

    add inventory

--- src/vehicleParser.py
+++ src/vehicleParser.py
@@ -5,6 +5,24 @@ from pprint import pp
 import genUtilities
 from settings import *
 
+def add_vehicle_inventory(file_path):
+    with open(file_path, "r") as json_file:
+        data = json.load(json_file)
+        item_name = data[0].get("items")
+        print("item_name")
+        #if not "" in item_name 
+
+def remove_unwanted_components(data):
+    keywords = ['slots', 'HeatSink', 'Tank', 'Tracked', 'Hover']
+    filtered_data = {}
+    for filename, components in data.items():
+        filtered_components = [
+            comp for comp in components
+            if 'ComponentDefID' in comp and not any(keyword in comp['ComponentDefID'] for keyword in keywords)
+        ]
+        filtered_data[filename] = filtered_components
+    return filtered_data
+
 def process_vehicle_files(directory):
     vehicle_dict = {}
     for root, _, files in os.walk(directory):
@@ -14,13 +32,12 @@ def process_vehicle_files(directory):
                 with open(file_path, 'r') as f:
                     data = json.load(f)
                     if "unit_controllableTank" in data["VehicleTags"]["items"]:
-                        print(f"Vehicle is {file_path} and is controllable")
-                        #vehicle_entry = parse_vehicle_json(file_path)
-                        #vehicle_dict.update(vehicle_entry)
-
+                        # Key by UIName, not file_path?
+                        vehicle_dict[file_path] = data["inventory"]
+                        vehicle_dict = remove_unwanted_components(vehicle_dict)
     return vehicle_dict
 
 if __name__ == "__main__":
     #result = process_vehicle_files(vehicle_dir_list)
-    result = process_vehicle_files("/Users/amidatelion/git/amids-vers/BattleTech-Advanced/VIPAdvanced")
+    result = process_vehicle_files(bta_dir + "/VIPAdvanced")
     #pp(result)
